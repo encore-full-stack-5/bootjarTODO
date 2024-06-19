83c2a45ddd5549e1f2104ec2ccd3690a6776d19c
HuiSeokJeong <tobysug123@gmail.com>
2024-06-19 23:50:35 +0900

    [feat]todo수정- handler 생성

---
+++ src/todos/commands/handlers/update-todo.handler.ts
@@ -0,0 +1,32 @@
+import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Todo } from '../../entities/todos.entity';
+import { DropTodosEvent } from '../../events/impl/drop-todos.event';
+import { UpdateTodoCommand } from '../impl/update-todo.command';
+
+@CommandHandler(UpdateTodoCommand)
+export class UpdateTodoHandler implements ICommandHandler<UpdateTodoCommand> {
+  constructor(
+    @InjectRepository(Todo)
+    private readonly todoRepository: Repository<Todo>,
+    private readonly eventBus: EventBus,
+  ) {}
+
+  async execute(command: UpdateTodoCommand): Promise<Todo> {
+    const { todoId, updateTodoDto } = command;
+    const todo = await this.todoRepository.findOneBy({ todoId });
+    const beforeDate = todo.todoDate.toString();
+
+    todo.categoryId = updateTodoDto.categoryId;
+    todo.todoTitle = updateTodoDto.todoTitle;
+    todo.todoContent = updateTodoDto.todoContent;
+    todo.todoDate = new Date(updateTodoDto.todoDate);
+    todo.todoDone = updateTodoDto.todoDone;
+
+    const result = await this.todoRepository.save(todo);
+    this.eventBus.publish(new DropTodosEvent(todo.userId, beforeDate));
+
+    return result;
+  }
+}
