b132ad51c2b64e790b7541267a7aba01710cb00a
HuiSeokJeong <tobysug123@gmail.com>
2024-06-20 00:18:30 +0900

    [feat]todo삭제- handler 주입

--- src/todos/commands/handlers/index.ts
+++ src/todos/commands/handlers/index.ts
@@ -1,4 +1,9 @@
 import { CreateTodoHandler } from './create-todo.handler';
 import { UpdateTodoHandler } from './update-todo.handler';
+import { DeleteTodoHandler } from './delete-todo.handler';
 
-export const CommandHandlers = [CreateTodoHandler, UpdateTodoHandler];
+export const CommandHandlers = [
+  CreateTodoHandler,
+  UpdateTodoHandler,
+  DeleteTodoHandler,
+];
